0e72a6a1e7fb6427c7f3593474a2538c5680396e
Zoran Pavlović <zpavlovicbj@gmail.com>
2021-03-20 09:34:39 +0100

    Proba

--- Financije.Presentation/Models/Financije/StoreViewModel.cs
+++ Financije.Presentation/Models/Financije/StoreViewModel.cs
@@ -5,7 +5,9 @@ namespace Financije.Presentation.Models.Financije
 {
     public class StoreViewModel
     {
-        [Required]
+        public int StoresId { get; set; }
+
+        [Required(ErrorMessage ="Potrebno je unesti naziv.")]
         [Display(Name = "Naziv")]
         public string StoreName { get; set; }
 
